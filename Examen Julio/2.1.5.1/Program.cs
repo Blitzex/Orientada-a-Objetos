5d1cd12f123e5808dcb72dececb4457583d437b8
Javier <blitzexgm@gmail.com>
2019-06-16 12:12:23 +0200

    Tema 2 terminado

--- Examen Julio/2.1.5.1/Program.cs
+++ Examen Julio/2.1.5.1/Program.cs
@@ -6,16 +6,23 @@ namespace _2._1._5._1
     {
         static void Main(string[] args)
         {
-            Console.Write("Introduce un número: ");
-            int num = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.Write("Introduce un número: ");
+                int num = Convert.ToInt32(Console.ReadLine());
 
-            if ((num % 2 == 0) || (num % 3 == 0))
-            {
-                Console.WriteLine("El número es múltiplo de 2 o 3");
+                if ((num % 2 == 0) || (num % 3 == 0))
+                {
+                    Console.WriteLine("El número es múltiplo de 2 o 3");
+                }
+                else
+                {
+                    Console.WriteLine("El número no es múltiplo de 2 o 3");
+                }
             }
-            else
+            catch (Exception errorEncontrado)
             {
-                Console.WriteLine("El número no es múltiplo de 2 o 3");
+                Console.WriteLine("Se ha producido un error: {0}", errorEncontrado.Message);
             }
             Console.ReadLine();
         }
--- Examen Julio/2.6.6/Program.cs
+++ Examen Julio/2.6.6/Program.cs
@@ -10,6 +10,21 @@ namespace _2._6._6
     {
         static void Main(string[] args)
         {
+            Console.Write("Introduce el ancho: ");
+            int ancho = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Introduce el alto: ");
+            int alto = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < alto; i++)
+            {
+                Console.Write("*");
+                for (int j = 0; j < ancho - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
