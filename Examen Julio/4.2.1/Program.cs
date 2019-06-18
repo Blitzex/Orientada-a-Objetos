c9b828e9d99d4c262729213e40ca4feb7225bb4b
Javier <blitzexgm@gmail.com>
2019-06-18 21:53:10 +0200

    Arrays bidimensionales hechos

---
+++ Examen Julio/4.2.1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numeros = new int[2, 10];
+            for(int i=0; i < 2; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    Console.Write("Introduce el {0}º número del {1}º array: ", j + 1, i + 1);
+                    numeros[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            int mayor1 = 0;
+            int mayor2 = 0;
+            int mayortotal = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                if (numeros[0, i] > mayor1)
+                {
+                    mayor1 = numeros[0, i];
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (numeros[1, i] > mayor2)
+                {
+                    mayor2 = numeros[1, i];
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (numeros[i,j] > mayortotal)
+                    {
+                        mayortotal = numeros[i,j];
+                    }
+                }
+            }
+            Console.WriteLine("El mayor del primer array es {0}, el del segundo {1} y el mayor de ambos es {2}", mayor1, mayor2, mayortotal);
+            Console.ReadLine();
+        }
+    }
+}
